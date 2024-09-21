8535481c2ea2c546d6548626938bfb8117ce4f12
rtyildirim <tolga.yildirim@gmail.com>
2024-09-21 12:17:38 -0400

    Add more api functionality and Bedrock integration

--- lib/ai_engineer_tech_challenge-stack.ts
+++ lib/ai_engineer_tech_challenge-stack.ts
@@ -3,23 +3,39 @@ import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 
 export class AiEngineerTechChallengeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const bedrockModelId = 'anthropic.claude-3-haiku-20240307-v1:0'; //'meta.llama3-1-405b-instruct-v1:0';
 
     // Lambda function to handle inference and return responses
     const inferenceLambda = new lambda.Function(this, 'InferenceFunction', {
       runtime: lambda.Runtime.PYTHON_3_9, // Use Python 3.9
       handler: 'inference_handler.lambda_handler',
       code: lambda.Code.fromAsset('src/lambda'),
+      timeout: cdk.Duration.seconds(60),
+      environment: {
+        BEDROCK_MODEL_ID: bedrockModelId,
+      },
     });
 
+    // Create policy to allow access to Amazon Bedrock from lambda
+    const bedrockPolicy = new iam.PolicyStatement({
+      actions: [
+        "bedrock:InvokeModel",
+        "bedrock:InvokeModelWithResponseStream",
+      ],
+      resources: ["*"], // TODO: restrict to specific resources
+    });
+
+    inferenceLambda.addToRolePolicy(bedrockPolicy);
+
     //  API Gateway to manage API request-respansoes
-    const api = new apigateway.RestApi(this, 'EngineeringChallengeAPI');
+    const api = new apigateway.RestApi(this, 'EngineeringChallengeAPI', {});
 
     // Define /infer POST endpoint to handle requests
     const inferResource = api.root.addResource('infer');
--- src/lambda/inference_handler.py
+++ src/lambda/inference_handler.py
@@ -1,10 +1,96 @@
+import boto3
 import json
+import os
+from botocore.exceptions import ClientError
 
-def lambda_handler(event, context):
-    # Return example response
+
+VALID_FOCUS_VALUES = {"web", "academic", "math", "writing", "video", "social"}
+
+
+def invalid_request_response(message):
+    return {
+        "statusCode": 400,
+        "body": json.dumps({"error": message})
+    }
+
+def get_validation_error(data):
+    if "question" not in data or not isinstance(data["question"], str):
+        return "The 'question' field is required and must be a string."
+
+    if "focus" in data:
+        if not isinstance(data["focus"], str):
+            return "The 'focus' field must be a string if provided."
+        if data["focus"].lower() not in VALID_FOCUS_VALUES:
+            return "Invalid 'focus' value. Must be one of: {}".format(", ".join(VALID_FOCUS_VALUES))
+
+    if "attachment" in data:
+        if not isinstance(data["attachment"], str):
+            return "The 'attachment' field must be a string if provided."
+
+    return None
+
+def send_prompt(prompt, focus, temperature=0.5, max_tokens=512):
+   
+    model_id = os.environ.get('BEDROCK_MODEL_ID')
+
+    native_request = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": max_tokens,
+        "temperature": 0.5,
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": prompt}],
+            },
+        ],
+    }
+
+    if focus:
+        native_request["messages"][0]["content"].append({"type": "text", "text": "Focus: " + focus})
+
+    request = json.dumps(native_request)
+
+    client = boto3.client('bedrock-runtime')
+
+    try:
+        response = client.invoke_model(modelId=model_id, body=request)
+    except (ClientError, Exception) as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error: {str(e)}")
+        }
+    
+    model_response = json.loads(response["body"].read())
+
+    response_text = model_response["content"][0]["text"]
+
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'Hello, World - Inference',
+        'question': prompt,
+        'focus': focus,
+        'response_text': response_text
         })
     }
+
+def lambda_handler(event, context):
+    
+    if 'body' in event:
+        body = event['body']
+        try:
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return invalid_request_response('Invalid JSON in request body')
+    else:
+        return invalid_request_response('No body in request')
+    
+    validation_error = get_validation_error(data)
+    if validation_error:
+        return invalid_request_response(validation_error)
+    
+    question = data['question']
+    focus = data.get('focus', '')
+    attchment = data.get('attachment', '')
+
+    return send_prompt(question, focus)
+
